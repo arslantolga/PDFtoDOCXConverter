a83eb7eeb373b5e68d2251101515a58f4800e36b
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-06-01 11:11:49 +0300

    regulating

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ FONT = ('Helvetica', 12)
 window = tkinter.Tk()
 window.geometry("400x300")
 window.title("PDF to DOCX Converter")
+
+
 def choose():
     pdf_filename = filedialog.askopenfilenames()
-
-    ana_dizin = pdf_filename[0]
-    ayrilmis = ana_dizin.split("/")
-    file_name = ayrilmis[-1]
-    del ayrilmis[-1]
-    birlesik = "/".join(ayrilmis)
-    birlesik = birlesik + f"/{file_name}.docx"
-
+    main_path = pdf_filename[0]
+    divided_str = main_path.split("/")
+    file_name = divided_str[-1]
+    del divided_str[-1]
+    docx_path = "/".join(divided_str)
+    docx_path = docx_path + f"/{file_name}.docx"
     cv = Converter(pdf_filename[0])
-    cv.convert(birlesik)
+    cv.convert(docx_path)
     cv.close()
+
     result_label.config(text="File Created", font=FONT, pady=30)
     result_label.pack()
 
@@ -27,7 +28,7 @@ def choose():
 label = tkinter.Label(text="Choose Your PDF File", font=FONT, pady=30)
 label.pack()
 
-button = tkinter.Button(text="Choose",command=choose)
+button = tkinter.Button(text="Choose", command=choose)
 button.pack()
 
 result_label = tkinter.Label(text="", font=FONT, pady=30)
